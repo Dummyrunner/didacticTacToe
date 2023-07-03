1c478b255603c0bd32ee187d72672e6b728cac35
Daniel Bergmann <daniel.bergmann93@gmx.de>
2023-07-03 20:08:13 +0200

    complete methods to check whether party has won

--- game_dynamics_tictactoe.py
+++ game_dynamics_tictactoe.py
@@ -1,6 +1,8 @@
 from Party import Party
 from cartpt import CartPt
 from move_tictactoe import MoveTicTacToe
+from enum import Enum
+from axis import Axis
 
 
 class GameDynamicsTicTacToe:
@@ -36,11 +38,11 @@ class GameDynamicsTicTacToe:
         self.board.setValueAtCartesian(pt, party)
 
     @staticmethod
-    def cohesiveSeqInPartyList(party_list: list, party: Party) -> int:
+    def maxLenCohesiveSeqInPartyList(party_list: list, party_of_interest: Party) -> int:
         len_max_seq = 0
         current_seq_len = 0
-        for x in party_list:
-            if x.value == party.value:
+        for party in party_list:
+            if party.value == party_of_interest.value:
                 current_seq_len += 1
             else:
                 len_max_seq = max(len_max_seq, current_seq_len)
@@ -49,14 +51,90 @@ class GameDynamicsTicTacToe:
         current_seq_len = 0
         return len_max_seq
 
-    def cohesiveSeqInRowOfParty(self, row_idx: int, party: Party) -> int:
+    def maxLenCohesiveSeqInRowOfParty(self, row_idx: int, party: Party) -> int:
         rowvals_as_list = self.board.rowValsAsList(row_idx)
-        return GameDynamicsTicTacToe.cohesiveSeqInPartyList(rowvals_as_list, party)
+        return GameDynamicsTicTacToe.maxLenCohesiveSeqInPartyList(
+            rowvals_as_list, party
+        )
 
-    def cohesiveSeqInColOfParty(self, col_idx: int, party: Party) -> int:
+    def maxLenCohesiveSeqInColOfParty(self, col_idx: int, party: Party) -> int:
         colvals_as_list = self.board.colValsAsList(col_idx)
-        return GameDynamicsTicTacToe.cohesiveSeqInPartyList(colvals_as_list, party)
+        return GameDynamicsTicTacToe.maxLenCohesiveSeqInPartyList(
+            colvals_as_list, party
+        )
 
-    def hasWonGame(party):
-        # TODO
-        pass
+    def maxLenCohesiveSeqInMaindiagOfParty(self, diag_idx: int, party: Party) -> int:
+        maindiagvals_as_list = self.board.maindiagValsAsList(diag_idx)
+        return GameDynamicsTicTacToe.maxLenCohesiveSeqInPartyList(
+            maindiagvals_as_list, party
+        )
+
+    def maxLenCohesiveSeqInAntidiagOfParty(self, diag_idx: int, party: Party) -> int:
+        antidiagvals_as_list = self.board.antidiagValsAsList(diag_idx)
+        return GameDynamicsTicTacToe.maxLenCohesiveSeqInPartyList(
+            antidiagvals_as_list, party
+        )
+
+    def hasPartyWonAtAxisByIndex(self, axis_idx: int, axis: Axis, party: Party) -> bool:
+        if axis == Axis.ROW:
+            return (
+                self.maxLenCohesiveSeqInRowOfParty(axis_idx, party)
+                >= self.ROWSIZE_TO_WIN
+            )
+        elif axis == Axis.COL:
+            return (
+                self.maxLenCohesiveSeqInColOfParty(axis_idx, party)
+                >= self.ROWSIZE_TO_WIN
+            )
+        elif axis == Axis.MAINDIAG:
+            return (
+                self.maxLenCohesiveSeqInMaindiagOfParty(axis_idx, party)
+                >= self.ROWSIZE_TO_WIN
+            )
+        elif axis == Axis.ANTIDIAG:
+            return (
+                self.maxLenCohesiveSeqInAntidiagOfParty(axis_idx, party)
+                >= self.ROWSIZE_TO_WIN
+            )
+        else:
+            raise AttributeError(
+                "Non-plausible Axis! Expected: Axis.ROW, Axis.COL, Axis.MAINDIAG, Axis.ANTIDIAG"
+            )
+
+    def hasPartyWonAtAnyAxis(self, axis: Axis, party: Party) -> bool:
+        #     # TODO
+        board = self.board
+        range_of_row_indices = range(0, board.numOfRows())
+        range_of_col_indices = range(0, board.numOfCols())
+        range_of_maindiag_indices = range(-(board.numOfRows() - 1), board.numOfCols())
+        range_of_antidiag_indices = range(-board.numOfCols() + 1, board.numOfRows())
+        if axis == Axis.ROW:
+            enough_to_win = [
+                self.maxLenCohesiveSeqInRowOfParty(i, party) >= self.ROWSIZE_TO_WIN
+                for i in range_of_row_indices
+            ]
+        elif axis == Axis.COL:
+            enough_to_win = [
+                self.maxLenCohesiveSeqInColOfParty(i, party) >= self.ROWSIZE_TO_WIN
+                for i in range_of_col_indices
+            ]
+        elif axis == Axis.MAINDIAG:
+            enough_to_win = [
+                self.maxLenCohesiveSeqInMaindiagOfParty(i, party) >= self.ROWSIZE_TO_WIN
+                for i in range_of_maindiag_indices
+            ]
+
+        elif axis == Axis.ANTIDIAG:
+            enough_to_win = [
+                self.maxLenCohesiveSeqInAntidiagOfParty(i, party) >= self.ROWSIZE_TO_WIN
+                for i in range_of_antidiag_indices
+            ]
+        else:
+            raise AttributeError(
+                "Non-plausible Axis! Expected: Axis.ROW, Axis.COL, Axis.MAINDIAG, Axis.ANTIDIAG"
+            )
+        return any(enough_to_win)
+
+    def hasPartyWon(self, party: Party) -> bool:
+        relevant_axes = [Axis.ROW, Axis.COL, Axis.MAINDIAG, Axis.ANTIDIAG]
+        return any([self.hasPartyWonAtAnyAxis(axe, party) for axe in relevant_axes])
--- test_game_dynamics_tictactoe.py
+++ test_game_dynamics_tictactoe.py
@@ -5,9 +5,10 @@ from cartpt import CartPt
 from Party import Party
 from move_tictactoe import MoveTicTacToe
 
-from human_player_tictactoe import HumanPlayerTicTacToe
-
+from axis import Axis
 from game_dynamics_tictactoe import GameDynamicsTicTacToe
+
+from human_player_tictactoe import HumanPlayerTicTacToe
 
 
 @pytest.fixture
@@ -51,16 +52,144 @@ def test_n_of_party_cohesive_seq_in_row():
     state_string = "O_O\nOOO\n_OO"
     board = Board.fromString(state_string)
     dynamics = GameDynamicsTicTacToe(board, 111)
-    assert dynamics.cohesiveSeqInRowOfParty(0, Party.BLACK) == 1
-    assert dynamics.cohesiveSeqInRowOfParty(1, Party.BLACK) == 3
-    assert dynamics.cohesiveSeqInRowOfParty(2, Party.BLACK) == 2
+    assert dynamics.maxLenCohesiveSeqInRowOfParty(0, Party.BLACK) == 1
+    assert dynamics.maxLenCohesiveSeqInRowOfParty(1, Party.BLACK) == 3
+    assert dynamics.maxLenCohesiveSeqInRowOfParty(2, Party.BLACK) == 2
 
 
 def test_n_of_party_cohesive_seq_in_col():
     state_string = "OO_\n_OO\nOOO"
     board = Board.fromString(state_string)
-    print(board)
     dynamics = GameDynamicsTicTacToe(board, 111)
-    assert dynamics.cohesiveSeqInColOfParty(0, Party.BLACK) == 1
-    assert dynamics.cohesiveSeqInColOfParty(1, Party.BLACK) == 3
-    assert dynamics.cohesiveSeqInColOfParty(2, Party.BLACK) == 2
+    assert dynamics.maxLenCohesiveSeqInColOfParty(0, Party.BLACK) == 1
+    assert dynamics.maxLenCohesiveSeqInColOfParty(1, Party.BLACK) == 3
+    assert dynamics.maxLenCohesiveSeqInColOfParty(2, Party.BLACK) == 2
+
+
+def test_has_party_won_at_axis_rows():
+    line0 = "OXO_\n"
+    line1 = "OOO_\n"
+    line2 = "OOXO"
+    state_string = line0 + line1 + line2
+    board = Board.fromString(state_string)
+
+    rowsize_to_win = 3
+    dynamics = GameDynamicsTicTacToe(board, rowsize_to_win)
+
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.ROW, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.ROW, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.ROW, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.ROW, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.ROW, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.ROW, Party.WHITE) == False
+
+
+def test_has_party_won_at_axis_cols():
+    line0 = "OXO_\n"
+    line1 = "OOO_\n"
+    line2 = "OOXO"
+    state_string = line0 + line1 + line2
+    board = Board.fromString(state_string)
+
+    rowsize_to_win = 3
+    dynamics = GameDynamicsTicTacToe(board, rowsize_to_win)
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.COL, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.COL, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.COL, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(3, Axis.COL, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.COL, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.COL, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.COL, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(3, Axis.COL, Party.WHITE) == False
+
+
+def test_has_party_won_at_axis_maindiags():
+    line0 = "OXO_\n"
+    line1 = "XOX_\n"
+    line2 = "OXOX"
+    state_string = line0 + line1 + line2
+    board = Board.fromString(state_string)
+
+    rowsize_to_win = 3
+    dynamics = GameDynamicsTicTacToe(board, rowsize_to_win)
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.MAINDIAG, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.MAINDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.MAINDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.MAINDIAG, Party.WHITE) == True
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.MAINDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.MAINDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(3, Axis.MAINDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(3, Axis.MAINDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-1, Axis.MAINDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-1, Axis.MAINDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-2, Axis.MAINDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-2, Axis.MAINDIAG, Party.WHITE) == False
+
+
+def test_has_party_won_at_axis_antidiags():
+    line0 = "OXOX\n"
+    line1 = "XOX_\n"
+    line2 = "OXOX"
+    state_string = line0 + line1 + line2
+    board = Board.fromString(state_string)
+
+    rowsize_to_win = 3
+    dynamics = GameDynamicsTicTacToe(board, rowsize_to_win)
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.ANTIDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(0, Axis.ANTIDIAG, Party.WHITE) == True
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.ANTIDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(1, Axis.ANTIDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.ANTIDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(2, Axis.ANTIDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-1, Axis.ANTIDIAG, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAxisByIndex(-1, Axis.ANTIDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-2, Axis.ANTIDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-2, Axis.ANTIDIAG, Party.WHITE) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-3, Axis.ANTIDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAxisByIndex(-3, Axis.ANTIDIAG, Party.WHITE) == False
+
+
+def test_has_party_won_at_any_axis_antidiags():
+    line0 = "OXO_\n"
+    line1 = "OOO_\n"
+    line2 = "OOXO"
+    state_string = line0 + line1 + line2
+    board = Board.fromString(state_string)
+    rowsize_to_win = 3
+    dynamics = GameDynamicsTicTacToe(board, rowsize_to_win)
+
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.ROW, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.ROW, Party.WHITE) == False
+
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.COL, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.COL, Party.WHITE) == False
+
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.MAINDIAG, Party.BLACK) == False
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.MAINDIAG, Party.WHITE) == False
+
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.ANTIDIAG, Party.BLACK) == True
+    assert dynamics.hasPartyWonAtAnyAxis(Axis.ANTIDIAG, Party.WHITE) == False
+
+
+def test_has_party_won_game():
+    line0 = "OXO_\n"
+    line1 = "OOO_\n"
+    line2 = "OOXO"
+    state_string = line0 + line1 + line2
+    board = Board.fromString(state_string)
+    rowsize_to_win = 3
+    dynamics = GameDynamicsTicTacToe(board, rowsize_to_win)
+
+    assert dynamics.hasPartyWon(Party.BLACK) == True
+    assert dynamics.hasPartyWon(Party.WHITE) == False
+
+    line0 = "OXOX\n"
+    line1 = "X_OX\n"
+    line2 = "_OXX"
+    state_string = line0 + line1 + line2
+    board2 = Board.fromString(state_string)
+    rowsize_to_win = 3
+    dynamics2 = GameDynamicsTicTacToe(board2, rowsize_to_win)
+
+    assert dynamics2.hasPartyWon(Party.BLACK) == False
+    assert dynamics2.hasPartyWon(Party.WHITE) == True
