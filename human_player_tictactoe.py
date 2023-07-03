bc8b0158a0a051905a5a913abe7429f2b5d27d19
Daniel Bergmann <daniel.bergmann93@gmx.de>
2023-07-03 23:21:22 +0200

    begin to write player and input methods

--- human_player_tictactoe.py
+++ human_player_tictactoe.py
@@ -1,4 +1,6 @@
 from Party import Party
+from move_tictactoe import MoveTicTacToe
+from cartpt import CartPt
 
 
 class HumanPlayerTicTacToe:
@@ -15,6 +17,18 @@ class HumanPlayerTicTacToe:
             yield "Harald" + str(ctr)
             ctr += 1
 
-    def chooseMove(self):
-        # TODO
-        pass
+    def chooseMove(self) -> MoveTicTacToe:
+        print("Enter move for " + str(self.party) + " (Player " + self.name + "):")
+        ip_string = self.getKeyBoardInput()
+        return self.parseKeyboardInputToMove(ip_string)
+
+    def getKeyBoardInput(self) -> str:
+        ip = input(
+            "move: X,Y, where X is the line from 0 to NumOfLines-1, Y column from 0 to NumOfColumns-1"
+        )
+        return ip
+
+    def parseKeyboardInputToMove(self, ip: str):
+        coords = [int(x) for x in ip.split(",")]
+        x, y = coords
+        return CartPt(x, y)
