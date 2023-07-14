38a0316f09363fe2a90dc2c0658bd3b107fd9549
Daniel Bergmann <daniel.bergmann93@gmx.de>
2023-07-15 01:52:39 +0200

    make move choice on player side fallback safe topology wise

--- human_player_tictactoe.py
+++ human_player_tictactoe.py
@@ -19,13 +19,25 @@ class HumanPlayerTicTacToe:
             yield "Harald" + str(ctr)
             ctr += 1
 
-    def chooseMove(self) -> MoveTicTacToe:
+    def chooseMove(self, max_tries=2) -> MoveTicTacToe:
         print("party " + str(self.party))
         print("name: " + self.name)
-
-        print("Enter move for " + str(self.party) + " (Player " + self.name + "):")
-        ip_string = self.getKeyBoardInput()
-        return self.parseKeyboardInputToMove(ip_string)
+        for num_of_tries in range(0, max_tries):
+            print("num of tries: " + str(num_of_tries))
+            ip_string = self.getKeyBoardInput()
+            if self._inputTopologyValid(ip_string):
+                return self.parseKeyboardInputToMove(ip_string)
+            num_of_tries += 1
+            print(
+                "Input has not topology to represent move. try again! Bad topology input ("
+                + str(num_of_tries)
+                + "/"
+                + str(max_tries)
+                + ")"
+            )
+        raise ValueError(
+            "Repeatedly bad topology: Max num " + str(max_tries) + "exceeded"
+        )
 
     def _inputTopologyValid(self, ip_string: str, split_char=",") -> bool:
         """True, if input string ip_string contanins two integer values
