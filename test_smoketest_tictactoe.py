b4fa6c40b870b4b5be01cba720e24af91ae32a04
Daniel Bergmann <daniel.bergmann93@gmx.de>
2023-07-15 14:17:36 +0200

    write first smoketest

--- test_smoketest_tictactoe.py
+++ test_smoketest_tictactoe.py
@@ -1,6 +1,6 @@
 from game_execution import (
     GameExecution,
-    createDefaultTicTacToeGameExecutionHumanPlayers,
+    GameStatus,
 )
 from player_tictactoe import HumanPlayerTicTacToe
 from board import Board
@@ -8,19 +8,28 @@ from game_dynamics_tictactoe import GameDynamicsTicTacToe
 from move_tictactoe import MoveTicTacToe
 from cartpt import CartPt
 from Party import Party
+from conftest import ScriptedPlayer
 
 list_of_moves_white_white_wins = [
-    MoveTicTacToe(CartPt(1, 1), Party.WHITE),
-    MoveTicTacToe(CartPt(0, 0), Party.WHITE),
-    MoveTicTacToe(CartPt(2, 0), Party.WHITE),
-    MoveTicTacToe(CartPt(1, 0), Party.WHITE),
+    "1,1",
+    "0,0",
+    "2,0",
+    "1,0",
 ]
 
 list_of_moves_black_white_wins = [
-    MoveTicTacToe(CartPt(0, 1), Party.BLACK),
-    MoveTicTacToe(CartPt(2, 2), Party.BLACK),
-    MoveTicTacToe(CartPt(0, 2), Party.BLACK),
+    "0,1",
+    "2,2",
+    "0,2",
 ]
 
-
-ge = createDefaultTicTacToeGameExecutionHumanPlayers()
+pwhite = ScriptedPlayer(Party.WHITE, list_of_moves_white_white_wins)
+pblack = ScriptedPlayer(Party.BLACK, list_of_moves_black_white_wins)
+board = Board(3, 3)
+rowsize_to_win = 3
+dyn = GameDynamicsTicTacToe(board, rowsize_to_win)
+ge = GameExecution(dyn, pwhite, pblack)
+ge.executeGame()
+expected_status = GameStatus.WHITE_WINS
+actual_status = ge.status
+assert actual_status == expected_status
