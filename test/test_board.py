4c972b0368b62a36d127ce734aff33e19c4fbd5d
Daniel Bergmann <daniel.bergmann93@gmx.de>
2023-07-28 23:17:29 +0200

    split cohesion tests from board to separate file

--- test/test_board.py
+++ test/test_board.py
@@ -334,19 +334,3 @@ def test_row_string_to_formatted_short():
     testline = "XO"
     formatted = BoardRectangular._rowStringToFormatted(testline)
     assert testline == formatted
-
-
-def test_n_of_party_cohesive_seq_in_row():
-    state_string = "O_O\nOOO\n_OO"
-    board = BoardRectangular.fromString(state_string)
-    assert board.maxLenCohesiveSeqInRowOfParty(0, Party.BLACK) == 1
-    assert board.maxLenCohesiveSeqInRowOfParty(1, Party.BLACK) == 3
-    assert board.maxLenCohesiveSeqInRowOfParty(2, Party.BLACK) == 2
-
-
-def test_n_of_party_cohesive_seq_in_col():
-    state_string = "OO_\n_OO\nOOO"
-    board = BoardRectangular.fromString(state_string)
-    assert board.maxLenCohesiveSeqInColOfParty(0, Party.BLACK) == 1
-    assert board.maxLenCohesiveSeqInColOfParty(1, Party.BLACK) == 3
-    assert board.maxLenCohesiveSeqInColOfParty(2, Party.BLACK) == 2
