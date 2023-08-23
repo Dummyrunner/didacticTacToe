bf8c310aceb85114b2387551a2e83f73d6d4e9eb
Daniel Bergmann <daniel.bergmann93@gmx.de>
2023-08-24 01:06:36 +0200

    evolve Ai player concept

--- player_tictactoe_ai.py
+++ player_tictactoe_ai.py
@@ -4,16 +4,54 @@ from cartpt import CartPt
 from player_tictactoe import BasePlayer
 from .ai.tree.node import Node
 from .ai.tree.tree import Tree
+from game_dynamics import GameDynamicsBase
 
 
 class AiPlayerTicTacToe(BasePlayer):
-    def __init__(self, ai):
+    def __init__(self, party: Party, ai):
+        super().__init__(party)
         self.ai = ai
 
     def chooseMove(self) -> MoveTicTacToe:
-        raise NotImplementedError
+        return self.ai.chooseMove(self.board)
 
 
-class AiTicTacToeBase:
-    def chooseMove(board, whos_turn: Party, party: Party):
+class AiBase:
+    def __init__(self, dynamics):
+        self._game_dynamics = dynamics
+        self._whos_turn = 0
+
+    @property
+    def dynamics(self):
+        return self._game_dynamics
+
+    @property
+    def whosTurn(self):
+        return self._whos_turn
+
+    def setWhosTurn(self, party: Party):
+        self._whos_turn = party
+
+    def chooseMove(self, board, whos_turn: Party, party: Party):
+        return self._calculateBestMove(self)
+
+    def _calculateBestMove(self):
         raise NotImplementedError
+
+    def _buildDecisionTree(self, board, whos_turn):
+        game_state = {"board_state": board, "whos_turn": whos_turn}
+        current_state_node = Node(game_state)
+        admissible_moves = self.dynamics.admissibleMoves()
+        for move in admissible_moves:
+            # TODO
+            # resulting_game_state = ...
+            # current_state_node.addChild(resulting_game_state)
+            break
+        # get admissible moves
+        # for each admissible move, calculate new state
+        # build subtrees recursively
+        raise NotImplementedError
+
+    @staticmethod
+    def boardAndWhosTurnToDict(board, whos_turn):
+        return {"board_state": board, "whos_turn": whos_turn}
